7c82d040b6d2d854c0f3855da6cbb0d24d51e9e1
RMSAJ <92260209+RMSAJ@users.noreply.github.com>
2021-12-02 10:40:44 +0300

    my version

--- app/src/main/java/com/example/emptybattery/MainActivity.kt
+++ app/src/main/java/com/example/emptybattery/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.emptybattery
 
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -7,60 +8,75 @@ import android.content.IntentFilter
 import android.os.BatteryManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
 import com.example.emptybattery.databinding.ActivityMainBinding
 
+
+
+private var binding: ActivityMainBinding? = null
+
 class MainActivity : AppCompatActivity() {
+ private var myBatteryState: BroadcastReceiver? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        Log.e("Tag", "main")
+        myBatteryState = MyBatteryState()
+    }
+    override fun onStart() {
+        super.onStart()
+        registerReceiver(myBatteryState, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
     }
 }
 
+open class MyBatteryState : BroadcastReceiver() {
+    private var binding: ActivityMainBinding? = null
+
+
+    override fun onReceive(context: Context?, intent: Intent? ) {
+        Log.e("Tag", "onReceive")
+        val batteryStatus: Intent? = IntentFilter(Intent.ACTION_BATTERY_CHANGED).let { ifilter ->
+            context?.registerReceiver(null, ifilter)
+        }
+        val status: Int = batteryStatus?.getIntExtra(BatteryManager.EXTRA_STATUS, -1) ?: -1
+        val isCharging: Boolean = status == BatteryManager.BATTERY_STATUS_CHARGING
+                || status == BatteryManager.BATTERY_STATUS_FULL
+        cheChargeState(isCharging)
+        val batteryPct: Float? = batteryStatus?.let { intent ->
+            val level: Int = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+            val scale: Int = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+            level * 100 / scale.toFloat()
+
+        }
+        checkpercentage(batteryPct)
+    }
+
+    fun checkpercentage(prc: Float?){
+
+        when{
+
+          prc!! > 95.00   -> {
+              Log.e("Tag", "$prc")
+              binding?.imageViewBattaryLevel?.setImageResource(R.drawable.ic_baseline_battery_full_24)}
+          prc < 30.00   ->
+        }
+    }
+
+        fun cheChargeState(chargeState: Boolean){
+            when(chargeState) {
+                chargeState ->  binding?.imageViewBattaryLevel?.setImageResource(R.drawable.ic_baseline_battery_charging_full_24)
+                else -> {binding?.imageViewBattaryLevel?.setImageResource(R.drawable.ic_baseline_battery_charging_full_24)}
+            }
+        }
+    }
 
 
 
-//    override fun onStart() {
-//        registerReceiver(mReceiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
-//        super.onStart()
-//    }
-
-//    override fun onStop() {
-//        unregisterReceiver(mReceiver)
-//        super.onStop()
-//    }
 
 
 
-
-
-//class PhoneBatteryState : BroadcastReceiver() {
-//
-//    override fun onReceive(context: Context, intent: Intent) {
-//        showBatteryLevel(intent)
-//        showBatteryChargingState(intent)
-//
-//    }
-
-
-//    private fun showBatteryChargingState(intent: Intent) {
-//        val status: Int = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1)
-//
-//        if (status == BatteryManager.BATTERY_STATUS_CHARGING) {
-//            binding?.textViewChargingState?.text = "Charging"
-//        } else {
-//            binding?.textViewChargingState?.text = "Not Charging"
-//        }
-//    }
-
-
-//    private fun showBatteryLevel(intent: Intent){
-//        if (intent.action == "android.intent.action.BATTERY_CHANGED") {
-//
-//            when (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)) {
-//                in 100 downTo 70 -> binding?.textViewBattaryLevel?.text = "Excellent"
-//                in 69 downTo 30 -> binding?.textViewBattaryLevel?.text = "Good"
-//                in 29 downTo 5 -> binding?.textViewBattaryLevel?.text = "Low"
-//            }
-//
-//        }
-//    }
